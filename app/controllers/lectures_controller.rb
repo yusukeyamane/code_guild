3d8b5994f979f4cdf2d537dd60e8fd2bb0c03948
naomooo <naomooo.official@gmail.com>
2016-02-29 08:46:20 +0900

    controllerにnew, createアクション追加

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -1,5 +1,20 @@
 class LecturesController < ApplicationController
+
   def index
     @lectures = Lecture.all
+  end
+
+  def new
+    @lecture = Lecture.new
+  end
+
+  def create
+    Lecture.create(lecture_params)
+    redirect_to :root
+  end
+
+  private
+  def lecture_params
+    params.require(:lecture).permit(:title, :content, :total_avarable_time, :charge).merge(user_id: current_user.id)
   end
 end
