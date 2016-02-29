a56eef8f98e9ac25300566cfcb7c0b5aa58fda18
naomooo <naomooo.official@gmail.com>
2016-02-29 11:55:10 +0900

    showアクションへのルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "lectures#index"
   devise_for :users
   resources :quesitons
-  resources :lectures, only: [:index, :new, :create]
+  resources :lectures, only: [:index, :new, :create, :show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
