56b2399d919c38c8ee64fa12dc38aa4b6150d563
yusukeyamane <yusukeyamane01@gmail.com>
2016-03-02 22:20:13 +0900

    reviewsコントローラーを作成

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,28 @@
 class ReviewsController < ApplicationController
+
+  def show
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  private
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
+
+  def create_params
+  end
+
 end
