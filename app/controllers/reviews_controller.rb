01baee1d3fc172923dc2b4ff546e50028d0ba0cd
yusukeyamane <yusukeyamane01@gmail.com>
2016-03-03 18:28:04 +0900

    reviewのnew/createアクションを定義

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,19 +1,26 @@
 class ReviewsController < ApplicationController
 
-  def show
-  end
+  before_action :set_review, only: [:edit, :update]
+  before_action :set_contract, only: [:new, :create]
 
   def new
-    @review = Review.new
+    @review = current_user.reviews.new
+  end
+
+  def create
+    @review = current_user.reviews.new(review_params)
+    if @review.save
+      redirect_to :root, flash: { success: "評価を投稿しました" }
+    else
+      render :new, flash: { success: "評価が正しく投稿されませんでした" }
+    end
   end
 
   def edit
   end
 
-  def update
-  end
 
-  def create
+  def update
   end
 
   private
@@ -22,7 +29,12 @@ class ReviewsController < ApplicationController
     @review = Review.find(params[:id])
   end
 
-  def create_params
+  def set_contract
+    @contract = Contract.find(params[:contract_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content).merge(rate: params[:rate], contract_id: params[:contract_id])
   end
 
 end
