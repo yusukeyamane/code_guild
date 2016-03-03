74f1f88158f12eea20a4162a5624f64fd2e1a0af
yusukeyamane <yusukeyamane01@gmail.com>
2016-03-03 22:50:10 +0900

    userのマイページでreviewを表示できるようにした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
 
   def show
+    @reviews = Review.where(user_id: params[:id])
   end
 
   def edit
