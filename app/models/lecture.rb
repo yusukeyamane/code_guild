4a0d119f4496f2657ab07a971a122a752c0bfb30
yusukeyamane <yusukeyamane01@gmail.com>
2016-03-09 17:36:05 +0900

    Merge branch 'master' into create_paymaent_function

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -2,5 +2,6 @@ class Lecture < ActiveRecord::Base
   has_one :contract, as: :contractable
   belongs_to :user
 
-   acts_as_taggable_on :categories
+  acts_as_taggable_on :categories
+  validates_presence_of :title, :content, :charge
 end
