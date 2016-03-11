8bcd0db53eb70eb875c4c16eca7f0556454d2a72
naomooo <naomooo.official@gmail.com>
2016-03-11 19:28:22 +0900

    ・viewの記述をform_forにしたのに合わせてコントローラの記述も変更 ・ストロングパラメーターも追加

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,5 +1,6 @@
 class ChatsController < ApplicationController
   def index
+    @chat = Chat.new
     @chats = Chat.where(contract_id: params[:contract_id])
     @contract = Contract.find(params[:contract_id])
     config_pusher = YAML.load_file('config/pusher.yml')[Rails.env]
@@ -7,14 +8,17 @@ class ChatsController < ApplicationController
   end
 
   def create
-    Chat.create(message: params[:message], user_id: current_user.id, contract_id: params[:contract_id])
-    # 発行するPusherのチャンネルとイベント名を指定。それに紐づくプロパティ（キーとバリュー）を指定。
-    # プロパティのバリューはViewで「data.キー名」と記述することで取り出せる
-    Pusher.trigger('general_channel', 'chat_event', {
-      message: params[:message],
-      username: current_user.name,
-      contract_id: params[:contract_id]
-    })
-    render :text => 'OK', :status => 200
+    @chat = Chat.new(chat_params)
+    if @chat.save
+      # 発行するPusherのチャンネルとイベント名を指定。それに紐づくプロパティ（キーとバリュー）を指定。
+      # プロパティのバリューはViewで「data.キー名」と記述することで取り出せる
+      Pusher.trigger('general_channel', 'chat_event', chat_params)
+      render :text => 'OK', :status => 200
+    end
+  end
+
+  private
+  def chat_params
+    params.require(:chat).permit(:message).merge(user_id: current_user.id, contract_id: params[:contract_id])
   end
 end
