6692ced12d0e77e7dac31df22d5d0fc950b3520b
naomooo <naomooo.official@gmail.com>
2016-03-12 18:48:41 +0900

    skillモデルとskill_relationモデルとのリレーションの作成

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,6 +1,7 @@
 class Lecture < ActiveRecord::Base
   has_one :contract, as: :contractable
   belongs_to :user
+  has_many :skill_relations, as: :skill_relationable
 
   acts_as_taggable_on :categories
   validates_presence_of :title, :content, :charge
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ActiveRecord::Base
   has_one :contract, as: :contractable
+  has_many :skill_relations, as: :skill_relationable
   belongs_to :user
 
   validates_presence_of :title, :content, :reward
