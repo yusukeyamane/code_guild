aec2c8f3e1d580aae57a308e69500cf411835c5e
naomooo <naomooo.official@gmail.com>
2016-03-12 16:25:25 +0900

    リアルタイム更新でユーザーネームが正しく表示されるよう修正

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -12,7 +12,7 @@ class ChatsController < ApplicationController
     if @chat.save
       # 発行するPusherのチャンネルとイベント名を指定。それに紐づくプロパティ（キーとバリュー）を指定。
       # プロパティのバリューはViewで「data.キー名」と記述することで取り出せる
-      Pusher.trigger('general_channel', 'chat_event', chat_params)
+      Pusher.trigger('general_channel', 'chat_event', chat_params.merge(username: current_user.name))
       render :text => 'OK', :status => 200
     end
   end
