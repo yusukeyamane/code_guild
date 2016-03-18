bb92703f617dadf1ab36b290a9bc9c2e8e124086
yusukeyamane <yusukeyamane01@gmail.com>
2016-03-18 23:03:42 +0900

    nest関係を定義

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -2,6 +2,7 @@ class Lecture < ActiveRecord::Base
   has_one :contract, as: :contractable
   belongs_to :user
   has_many :skill_relations, as: :skill_relationable
+  accepts_nested_attributes_for :skill_relations
 
   validates_presence_of :title, :content, :charge
 end
