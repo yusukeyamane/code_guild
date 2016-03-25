9e8c9e8c8c0d74741d043fc1f17f71688f04eabd
naomooo <naomooo.official@gmail.com>
2016-03-26 03:28:09 +0900

    userコントローラーのshowのViewに使用する、そのユーザーのQuestion/Lectureそれぞれのレビューに対する平均値と、それぞれの合計投稿数を導くためのコードが長くなるため、モデル内にメソッドして作成した

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,5 +1,54 @@
 class Contract < ActiveRecord::Base
   has_many :chats
+  has_one :review
   belongs_to :contractable, polymorphic: true
   belongs_to :user
+
+  def self.lecture_review_rate_avarage(user_id)
+
+    rate_counter = []
+    sum = 0
+
+    if self.where(host_user_id: user_id, contractable_type: :Lecture).present?
+      self.where(host_user_id: user_id, contractable_type: :Lecture).each do |contract|
+        if contract.review
+          rate_counter << contract.review.rate
+          sum += contract.review.rate
+        end
+      end
+      return (sum / rate_counter.count / 2).round
+    else
+      return 0
+    end
+  end
+
+  def self.number_of_lecture_review(user_id)
+    self.where(host_user_id: user_id, contractable_type: :Lecture).count
+  end
+
+
+
+  def self.question_review_rate_avarage(user_id)
+
+    rate_counter = []
+    sum = 0
+
+    if self.where(guest_user_id: user_id, contractable_type: :Question).present?
+      self.where(guest_user_id: user_id, contractable_type: :Question).each do |contract|
+        if contract.review
+          rate_counter << contract.review.rate
+          sum += contract.review.rate
+        end
+      end
+      return (sum / rate_counter.count / 2).round
+    else
+      return 0
+    end
+
+  end
+
+  def self.number_of_question_review(user_id)
+    self.where(guest_user_id: user_id, contractable_type: :Question).count
+  end
+
 end
