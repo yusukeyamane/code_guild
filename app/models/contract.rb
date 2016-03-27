557a6aa8a1e0ab11d825e7197cc06deb9c858d90
naomooo <naomooo.official@gmail.com>
2016-03-27 20:26:33 +0900

    contractモデル内のメソッドの量が多くなったので、コメントでわかりやすくした。 また新しくcontracts#index内の部分テンプレートで使用するインスタントメソッドも作成

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -4,6 +4,7 @@ class Contract < ActiveRecord::Base
   belongs_to :contractable, polymorphic: true
   belongs_to :user
 
+# 注目しているユーザーのlectureに対するレビューの平均値を1~5で取り出す
   def self.lecture_review_rate_avarage(user_id)
 
     rate_counter = []
@@ -26,12 +27,13 @@ class Contract < ActiveRecord::Base
     end
   end
 
+  #注目しているユーザーのlectureに対して投稿されているレビューの総数を導き出す
   def self.number_of_lecture_review(user_id)
     where(host_user_id: user_id, contractable_type: :Lecture).count
   end
 
 
-
+  # 注目しているユーザーが回答した質問に対するレビューの平均値を1~5で取り出す
   def self.question_review_rate_avarage(user_id)
 
     rate_counter = []
@@ -55,8 +57,80 @@ class Contract < ActiveRecord::Base
 
   end
 
+  #注目しているユーザーが回答した質問に対して投稿されているレビューの総数を導き出す
   def self.number_of_question_review(user_id)
     where(guest_user_id: user_id, contractable_type: :Question).count
   end
+
+
+
+
+
+
+
+# 注目しているユーザーのlectureに対するレビューの平均値を1~5で取り出す
+  def lecture_review_rate_avarage
+
+    rate_counter = []
+    sum = 0
+
+    if Contract.where(host_user_id: contractable.user_id, contractable_type: :Lecture).present?
+      Contract.where(host_user_id: contractable.user_id, contractable_type: :Lecture).each do |c|
+        if c.review
+          rate_counter << c.review.rate
+          sum += c.review.rate
+        end
+      end
+      unless rate_counter.count == 0
+        return (sum / rate_counter.count / 2).round
+      else
+        return 0
+      end
+    else
+      return 0
+    end
+  end
+
+  #注目しているユーザーのlectureに対して投稿されているレビューの総数を導き出す
+  def number_of_lecture_review
+    Contract.where(host_user_id: contractable.user_id, contractable_type: :Lecture).count
+  end
+
+
+  # 注目しているユーザーが回答した質問に対するレビューの平均値を1~5で取り出す
+  def question_review_rate_avarage
+
+    rate_counter = []
+    sum = 0
+
+    if Contract.where(guest_user_id: contractable.user_id, contractable_type: :Question).present?
+      Contract.where(guest_user_id: contractable.user_id, contractable_type: :Question).each do |c|
+        if c.review
+          rate_counter << c.review.rate
+          sum += c.review.rate
+        end
+      end
+      unless rate_counter.count == 0
+        return (sum / rate_counter.count / 2).round
+      else
+        return 0
+      end
+    else
+      return 0
+    end
+
+  end
+
+  #注目しているユーザーが回答した質問に対して投稿されているレビューの総数を導き出す
+  def number_of_question_review
+    Contract.where(guest_user_id: contractable.user_id, contractable_type: :Question).count
+  end
+
+
+
+
+
+
+
 
 end
