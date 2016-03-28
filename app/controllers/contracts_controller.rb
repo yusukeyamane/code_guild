62c0cad7d66de7e5220386b356ebefe155cd981a
naomooo <naomooo.official@gmail.com>
2016-03-29 03:05:39 +0900

    自分に関係ない契約が表示されるバグを修正

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -13,16 +13,29 @@ class ContractsController < ApplicationController
   def search
 
     if params[:category_form] == "All"
+
       if params[:usertype_form] == "usertype_all"
-        @contracts = Contract.where(situation: params[:contract_situation_form])
+        @contracts = Contract.where(host_user_id: current_user.id, situation: params[:contract_situation_form])
+        if Contract.where(guest_user_id: current_user.id ,situation: params[:contract_situation_form]).present?
+          Contract.where(guest_user_id: current_user.id ,situation: params[:contract_situation_form]).each do |c|
+            @contracts << c
+          end
+        end
+
       elsif params[:usertype_form] == "usertype_host"
         @contracts = Contract.where(host_user_id: current_user.id, situation: params[:contract_situation_form])
       elsif params[:usertype_form] == "usertype_guest"
         @contracts = Contract.where(guest_user_id: current_user.id ,situation: params[:contract_situation_form])
       end
+
     else
       if params[:usertype_form] == "usertype_all"
-        @contracts = Contract.where(contractable_type: params[:category_form], situation: params[:contract_situation_form])
+       @contracts = Contract.where(contractable_type: params[:category_form], host_user_id: current_user.id, situation: params[:contract_situation_form])
+        if Contract.where(contractable_type: params[:category_form], guest_user_id: current_user.id ,situation: params[:contract_situation_form]).present?
+          Contract.where(contractable_type: params[:category_form], guest_user_id: current_user.id ,situation: params[:contract_situation_form]).each do |c|
+            @contracts << c
+          end
+        end
       elsif params[:usertype_form] == "usertype_host"
         @contracts = Contract.where(contractable_type: params[:category_form], host_user_id: current_user.id, situation: params[:contract_situation_form])
       elsif params[:usertype_form] == "usertype_guest"
