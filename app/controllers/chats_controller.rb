ba48264c1e39c9ae03a784c9aacfd12ee19a8631
naomooo <naomooo.official@gmail.com>
2016-03-29 03:09:41 +0900

    chatsコントローラー

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,8 +1,23 @@
 class ChatsController < ApplicationController
+
   def index
+    @contracts = Contract.where(host_user_id: current_user.id, situation: "doing")
+    if Contract.where(guest_user_id: current_user.id ,situation: "doing").present?
+      Contract.where(guest_user_id: current_user.id ,situation: "doing").each do |c|
+        @contracts << c
+      end
+    end
+  end
+
+  def show
     @chat = Chat.new
-    @chats = Chat.where(contract_id: params[:contract_id])
-    @contract = Contract.find(params[:contract_id])
+    @chats = Chat.where(contract_id: params[:id])
+    @contract = Contract.find(params[:id])
+    if @contract.host_user_id == current_user.id
+      @another_user = User.find(@contract.guest_user_id)
+    else
+      @another_user = User.find(@contract.host_user_id)
+    end
     config_pusher = YAML.load_file('config/pusher.yml')[Rails.env]
     @pusher_access_key = config_pusher['access_key']
   end
@@ -12,13 +27,25 @@ class ChatsController < ApplicationController
     if @chat.save
       # 発行するPusherのチャンネルとイベント名を指定。それに紐づくプロパティ（キーとバリュー）を指定。
       # プロパティのバリューはViewで「data.キー名」と記述することで取り出せる
-      Pusher.trigger('general_channel', 'chat_event', chat_params.merge(username: current_user.name))
+      Pusher.trigger('general_channel', 'chat_event', chat_params.merge(user_id: current_user.id, username: current_user.name, avatar: current_user.avatar))
       render :text => 'OK', :status => 200
+    end
+  end
+
+  def search
+    @contracts = Contract.where(host_user_id: current_user.id, situation: params[:contract_situation_form])
+    if Contract.where(guest_user_id: current_user.id ,situation: params[:contract_situation_form]).present?
+      Contract.where(guest_user_id: current_user.id ,situation: params[:contract_situation_form]).each do |c|
+        @contracts << c
+      end
     end
   end
 
   private
   def chat_params
-    params.require(:chat).permit(:message).merge(user_id: current_user.id, contract_id: params[:contract_id])
+    params.require(:chat).permit(:message, :contract_id).merge(user_id: current_user.id)
+  end
+  def create_params
+    params..require(:chat).permit(:contract_id)
   end
 end
