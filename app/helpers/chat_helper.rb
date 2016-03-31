85a452bcc55759f44e30952102d7b91133ec0f34
yusukeyamane <yusukeyamane01@gmail.com>
2016-03-31 18:28:28 +0900

    ボタンを出すタイミングの判定をメソッド化

--- app/helpers/chat_helper.rb
+++ app/helpers/chat_helper.rb
@@ -1,2 +1,9 @@
 module ChatHelper
+  def button_should_appeared?
+    if current_user.id == @contract.host_user_id && @contract.situation == "will_do"
+      true
+    else
+      false
+    end
+  end
 end
